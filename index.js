086d4f5e0222f5dee977ade89cf1029208b0a00e
Tony Lownds <tony@lownds.com>
2019-05-23 11:21:31 -0700

    Wording tweak

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const {app, setMode} = require('./app');
 
 require('fncli')(function (mode='proxy', {port=8888}) {
   setMode(mode);
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  app.listen(port, () => console.log(`apirec listening on port ${port}!`))
 });
